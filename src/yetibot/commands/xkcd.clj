566e3526b54df41f1b05d32507ec3e85f4c23b5b
audaxion <audaxion@gmail.com>
2019-03-29 15:01:22 -0700

    Add ability to show xkcd by id

--- src/yetibot/commands/xkcd.clj
+++ src/yetibot/commands/xkcd.clj
@@ -3,13 +3,22 @@
     [yetibot.core.util.http :refer [get-json]]
     [yetibot.core.hooks :refer [cmd-hook]]))
 
-(def endpoint "http://xkcd.com/info.0.json")
+(defn endpoint
+  ([] ("http://xkcd.com/info.0.json"))
+  ([num] (str "http://xkcd.com/" num "/info.0.json")))
 
-(defn xkcd-cmd
+(defn xkcd-current-cmd
   "xkcd # fetch current xkcd comic"
   {:yb/cat #{:fun :img}}
   [_]
-  ((juxt :title :img :alt) (get-json endpoint)))
+  ((juxt :title :img :alt) (get-json (endpoint))))
+
+(defn xkcd-cmd
+  "xkcd <num> # fetch specific xkcd comic"
+  [{[_ num] :match}]
+  ((juxt :title :img :alt) (get-json (endpoint num))))
 
 (cmd-hook #"xkcd"
-          _ xkcd-cmd)
+          #"^$" xkcd-current-cmd
+          #"^(\d+)" xkcd-cmd
+          _ xkcd-current-cmd)
